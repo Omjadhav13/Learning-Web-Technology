9924b877bfd433f9304213eb55a1e0a69bd77e0a
Omjadhav13 <omjadhav1303@gmail.com>
2025-09-28 12:38:50 +0530

    Module for Area fo Circle

---
+++ Day_7_Nodejs/CircleModule.js
@@ -0,0 +1,3 @@
+const modulue=require('./areaofcircle');
+console.log(modulue.name)
+console.log(modulue.AOC(2));
---
+++ Day_7_Nodejs/areaofcircle.js
@@ -0,0 +1,7 @@
+
+const name='Area of Circle'
+
+const AOC=(num)=>{
+    return 3.14*num*num;
+}
+module.exports={name,AOC};
