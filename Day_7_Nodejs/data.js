0bbbcd5391e0258ffdd6570fb8014423d29e785b
Omjadhav13 <omjadhav1303@gmail.com>
2025-09-28 12:22:23 +0530

    Custom Module

---
+++ Day_7_Nodejs/data.js
@@ -0,0 +1,18 @@
+let myModulue='om .s. jadhav';
+
+
+const add=(num1,num2)=>{
+    return num1+num2;
+}
+
+const sub=(num1,num2)=>{
+    return num1-num2;
+}
+const mul=(num1,num2)=>{
+    return num1*num2;
+}
+const div=(num1,num2)=>{
+    return num1/num2;
+}
+
+module.exports={myModulue,add,mul,sub,div};
---
+++ Day_7_Nodejs/module.js
@@ -0,0 +1,6 @@
+const myData=require('./data.js');
+console.log(myData.myModulue);
+console.log(myData.add(2,3));
+console.log(myData.sub(2,3));
+console.log(myData.mul(2,3));
+console.log(myData.div(2,3));
