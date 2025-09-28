1f7379575eeb837b50510715e0b554c285a36c4b
Omjadhav13 <omjadhav1303@gmail.com>
2025-09-28 10:42:51 +0530

    Add Day 7 Node.js files

---
+++ Day_7_Nodejs/demo.js
@@ -0,0 +1,12 @@
+let fname="om",lname="jadhav"
+fname+" "+lname
+function test(std){
+return "welcome you"+std+"in AIT";
+}
+test(fname +" "+lname)
+test(fname +" "+lname)
+
+
+const add=(num1,num2)=>{
+return num1+num2;
+}
