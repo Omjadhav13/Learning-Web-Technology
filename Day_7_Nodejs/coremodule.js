61315ea968bc644e9fef199424f0f9d7f98375ba
Omjadhav13 <omjadhav1303@gmail.com>
2025-09-29 16:58:30 +0530

    core node_modules

--- Day_7_Nodejs/coremodule.js
+++ Day_7_Nodejs/coremodule.js
@@ -1,6 +1,16 @@
 const path=require('path');
+const os=require('os');
+
 console.log(__dirname);
 console.log(__filename);
 console.log(path.basename(__dirname));
+console.log(path.extname(__filename));
+console.log(path.isAbsolute(__filename));
+console.log(path.isAbsolute('../'+__filename));
+console.log(path.join(__dirname,'MyDir'));
+
+console.log(os.arch());
+console.log(os.machine());
+
 
 
