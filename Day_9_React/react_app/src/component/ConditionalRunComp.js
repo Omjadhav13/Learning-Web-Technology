80a704eefbebe7f04ddb8ab795799cbd71e65bd3
Omjadhav13 <omjadhav1303@gmail.com>
2025-09-30 15:06:57 +0530

    Conditional rendering

--- Day_9_React/react_app/src/component/ConditionalRunComp.js
+++ Day_9_React/react_app/src/component/ConditionalRunComp.js
@@ -10,13 +10,23 @@ export class ConditionalRunComp extends Component {
     }
     
     render() {
-            // 1. use of if else
-            if(this.isCon){
-                return <h2>Admin Login</h2>
-            }
-            else{
-                return <h2>User Login</h2>
-            }
+        // let msg="";    
+        // // 1. use of if else
+        // if(this.state.isCon){
+        //     msg ="Admin login"
+        // }
+        // else{
+        //     msg= "User Login"
+        //    }
+
+        // //2. use of element as variable
+        // return <h2>{msg}</h2>
+
+        //3. use of ternary operator
+        // return (this.state.isCon)?<h2>Admin Login</h2>:<h2>User login</h2>
+
+        // 4. use of short circuit
+        return this.state.isCon && <h2>Admin Login</h2>
         
     }
 }
